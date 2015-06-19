373e5e0c05f7a96c7493db258661ec988da06aed
mark hughes <mark.hughes4096@gmail.com>
2015-06-19 13:08:04 +0100

    updates

---
+++ ScrambleWords/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ScrambleWords
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Word = new System.Windows.Forms.TextBox();
+            this.lbl_EnterLetters = new System.Windows.Forms.Label();
+            this.btn_Go = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_Word
+            // 
+            this.txt_Word.Location = new System.Drawing.Point(16, 29);
+            this.txt_Word.Name = "txt_Word";
+            this.txt_Word.Size = new System.Drawing.Size(100, 20);
+            this.txt_Word.TabIndex = 0;
+            // 
+            // lbl_EnterLetters
+            // 
+            this.lbl_EnterLetters.AutoSize = true;
+            this.lbl_EnterLetters.Location = new System.Drawing.Point(13, 13);
+            this.lbl_EnterLetters.Name = "lbl_EnterLetters";
+            this.lbl_EnterLetters.Size = new System.Drawing.Size(86, 13);
+            this.lbl_EnterLetters.TabIndex = 1;
+            this.lbl_EnterLetters.Text = "Enter your letters";
+            // 
+            // btn_Go
+            // 
+            this.btn_Go.Location = new System.Drawing.Point(138, 26);
+            this.btn_Go.Name = "btn_Go";
+            this.btn_Go.Size = new System.Drawing.Size(75, 23);
+            this.btn_Go.TabIndex = 2;
+            this.btn_Go.Text = "GO!";
+            this.btn_Go.UseVisualStyleBackColor = true;
+            this.btn_Go.Click += new System.EventHandler(this.btn_Go_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 73);
+            this.Controls.Add(this.btn_Go);
+            this.Controls.Add(this.lbl_EnterLetters);
+            this.Controls.Add(this.txt_Word);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Word;
+        private System.Windows.Forms.Label lbl_EnterLetters;
+        private System.Windows.Forms.Button btn_Go;
+    }
+}
+
---
+++ ScrambleWords/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+
+
+namespace ScrambleWords
+{
+    public partial class Form1 : Form
+    {
+        List<string> Dictionary = new List<string>();        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Loads up all the words we will compare against.
+            PopulateDictionary();
+        }
+
+
+        private void btn_Go_Click(object sender, EventArgs e)
+        {
+            // Recover the letters from the dialog box
+            var Letters = txt_Word.Text.ToString();
+            //split the dialog string into individual letters
+            var letters = SplitString(Letters);
+            //find all the words with those letters in
+            var WordsWithTheseLetters = ReturnAllWordsWithTheseLettersIn(letters);
+            //find all the words that contain only those letters and no others.
+            var WordsWithONLYTheseLetters = ReturnAllWordsWithONLYTheseLettersIn(letters);
+
+            var AllLettersDifferent = AreAllTheLettersSelectedDifferent(letters);
+
+
+             if (AllLettersDifferent)
+            {
+                MessageBox.Show("Found no duplicate letters");
+            }
+            else
+            {
+                MessageBox.Show("Found duplicate letters");
+            
+            }
+
+            //MessageBox.Show("I found " + WordsWithTheseLetters.Count + " Words with these letters");
+            //MessageBox.Show("I found " + WordsWithONLYTheseLetters.Count + " Words with ONLY these letters");
+
+        }
+
+        /// <summary>
+        /// Find out if a list of letters contains a duplicate letter
+        /// </summary>
+        /// <param name="letters"></param>
+        /// <returns></returns>
+        public bool AreAllTheLettersSelectedDifferent(List<char> letters)
+        {
+            Dictionary<char, int> charcount = new Dictionary<char , int>();
+            //int charcount = 0;
+            //if (letters.)
+            foreach (char letter in letters)
+            {
+                if (letters.Contains(letter))
+                {
+                    foreach (KeyValuePair<char , int> kvp in charcount)
+                    {
+                        if (kvp.Key.Equals(letter))
+                        {
+
+                        }
+                    }
+                    //if (charcount.ContainsKey(letter))
+                    //{
+                    //    charcount.
+                   // }
+                }
+                //if (charcount > 1) //more than one instance of the letter found
+                //{
+                //    return false;
+                //}
+            }
+            return true;
+
+        }
+
+        public List<string> ReturnAllWordsWithTheseLettersIn(List<char> letters)
+        {
+            List<string> WordsWithTheseLetters = new List<string>();
+
+            foreach (string TestString in Dictionary)
+            {
+                var Pass = true;
+                foreach (char Letter in letters)
+                {
+                    if (!TestString.Contains(Letter))
+                    {
+                        Pass = false;
+                    }
+
+                }
+                if (Pass == true)
+                {
+                    WordsWithTheseLetters.Add(TestString);
+                }
+            }
+            return WordsWithTheseLetters;
+        }
+
+        public List<string> ReturnAllWordsWithONLYTheseLettersIn(List<char> letters)
+        {
+            List<string> WordsWithTheseLetters = new List<string>();
+
+            foreach (string TestString in Dictionary)
+            {
+                var Pass = true;
+                foreach (char Letter in letters)
+                {
+                    if (!TestString.Contains(Letter))
+                    {
+                        Pass = false;
+                    }
+
+                }
+                if (Pass == true && TestString.Length == letters.Count)
+                {
+                    WordsWithTheseLetters.Add(TestString);
+                }
+            }
+            return WordsWithTheseLetters;
+        }
+
+
+        public List<char> SplitString(string inputLetters)
+        {
+            var letters = new List<char>();
+
+            for (int n = 0; n < inputLetters.Length; n++)
+            {
+                letters.Add(inputLetters[n]);
+            }
+            return letters;
+        }
+
+
+
+
+
+        void PopulateDictionary()
+        {
+            List<string> duplicates = new List<string>();
+            string line = "";
+            //need to load file here
+           // System.IO.StreamReader file = new System.IO.StreamReader(MyDirectory() + @"\WordList_Eng_UK.txt");
+            System.IO.StreamReader file = new System.IO.StreamReader(MyDirectory() + @"\wordsEn.txt");
+          
+            while (!string.IsNullOrWhiteSpace(line = file.ReadLine()))
+            {
+                if (Dictionary.Contains(line))
+                {
+                    duplicates.Add(line);
+                }
+                else
+                {
+                    Dictionary.Add(line);
+                }
+            }
+            if (duplicates.Count > 0)
+            {
+                //MessageBox.Show("Duplicate words found " + duplicates.Count);
+            }
+            file.Close();
+
+
+        }
+
+        private string MyDirectory()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+    }
+}
